9b854c4066bfc2bac7d761523ebb4ac0c1cf514d
Joshua Griffith <jgriffith@campuslabs.com>
2016-11-22 15:51:44 -0600

    Make sure consul is up before loading backup

--- load.sh
+++ load.sh
@@ -7,6 +7,11 @@ if [ $1 == "backup" ]; then
   $CONSUL_TO_JSON backup -p $KV_SOURCE
 else
   $CONSUL_ENTRYPOINT "$@" &
+
+  until $(curl --output /dev/null --silent --fail http://127.0.0.1:8500/v1/status/leader); do
+    sleep 1
+  done
+
   $CONSUL_TO_JSON restore $KV_SOURCE
   wait $!
 fi
